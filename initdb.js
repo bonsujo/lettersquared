197b39cb60102204170178ca1e0637799ad9d817
Josephine <bonsujo@sheridancollege.ca>
2025-03-10 19:38:12 -0400

    Error: failing to lookup update_form.mustache

--- initdb.js
+++ initdb.js
@@ -4,6 +4,7 @@ const db = new sqlite3.Database("movies.db")
 db.serialize(function(){
     db.run("DROP TABLE IF EXISTS Reviews");
     db.run(`CREATE TABLE IF NOT EXISTS Reviews (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
         moviename TEXT NOT NULL,
         genre TEXT,
         review TEXT,
@@ -13,10 +14,22 @@ db.serialize(function(){
       )`);
 
 
-  db.run("INSERT INTO Reviews VALUES (?,?,?,?,?,?)", ['John Wick','Action','Why did they kill the dog!', '2021-11-29','5', 1 ]);
-  db.run("INSERT INTO Reviews VALUES (?,?,?,?,?,?)", ['Maze Runner','Action','Dylan O Brien is a heartthrob <3', '2025-02-24','4', 0 ]);
-  db.run("INSERT INTO Reviews VALUES (?,?,?,?,?,?)", ['Memories of a Murder','Thriller','I am rattled, amazing movie', '2013-02-11','5', 1 ]);
-  db.run("INSERT INTO Reviews VALUES (?,?,?,?,?,?)", ['Companion','Action','Great twist, did not know she was a robot', '2025-02-15','4', 0 ]);
-
+      db.serialize(function(){
+        db.run("DROP TABLE IF EXISTS Reviews");
+        db.run(`CREATE TABLE IF NOT EXISTS Reviews (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            moviename TEXT NOT NULL,
+            genre TEXT,
+            review TEXT,
+            date_watched DATE,
+            rating REAL,
+            favourite INTEGER
+        )`);
+    
+        db.run("INSERT INTO Reviews (moviename, genre, review, date_watched, rating, favourite) VALUES (?,?,?,?,?,?)", ['John Wick','Action','Why did they kill the dog!', '2021-11-29','5', 1 ]);
+        db.run("INSERT INTO Reviews (moviename, genre, review, date_watched, rating, favourite) VALUES (?,?,?,?,?,?)", ['Maze Runner','Action','Dylan O Brien is a heartthrob <3', '2025-02-24','4', 0 ]);
+        db.run("INSERT INTO Reviews (moviename, genre, review, date_watched, rating, favourite) VALUES (?,?,?,?,?,?)", ['Memories of a Murder','Thriller','I am rattled, amazing movie', '2013-02-11','5', 1 ]);
+        db.run("INSERT INTO Reviews (moviename, genre, review, date_watched, rating, favourite) VALUES (?,?,?,?,?,?)", ['Companion','Action','Great twist, did not know she was a robot', '2025-02-15','4', 0 ]);
+    });
 
 })
