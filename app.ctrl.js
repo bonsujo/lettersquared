b20b37d845a9a7195056933b18e453c446ebe246
Josephine <bonsujo@sheridancollege.ca>
2025-03-10 16:30:28 -0400

    Controller: functionality for getting all movies

--- app.ctrl.js
+++ app.ctrl.js
@@ -10,6 +10,13 @@ app.set('view engine', 'mustache');
 app.set('views', __dirname + '/views');
 
 
+// Show all movies and provide link to add a new movie
+app.get('/', async function(req, res) {
+    const movies = await Model.getAllMovies(); // Get all movies
+    res.render('movie_page', { movies: movies });
+  });
+
+
 // Start the server
 app.listen(3000, function() { 
     console.log("Server listening on port 3000..."); 
